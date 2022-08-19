8e16841abe82c879d0fcf3192a554867d50d84ee
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-19 12:45:03 -0400

    post message as soon as sender hits send

--- public/script.js
+++ public/script.js
@@ -14,7 +14,6 @@ socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
   typing.innerHTML = ''
-  post_message_data(data.message)
   //This name is the sender name; you are recipient here
   appendMessage(`${data.name}: ${data.message}`)
 })
@@ -47,6 +46,8 @@ socket.on('typing', data => {
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
+  //This name here is the sender name - so as soon as he hits the send, you send the message to the db
+  post_message_data(message)
   appendMessage(`${name}: ${message}`)
   socket.emit('send-chat-message', message)
   messageInput.value = ''
@@ -67,7 +68,7 @@ function post_message_data(message){
   var data = {
       "messages": {
           "chat_uuid" : "3b547d64-5c37-4f97-a7e2-4254bd9f06d0",
-          "chat_name" : "front-end",
+          "chat_name" : "front-end-version-2",
           "sender_uuid" : "15d18e55-f3c0-476b-bc05-ec12bc36780d",
           "receiver_uuid" : "f9748683-99f2-4246-b069-7f001ff1b3a4",
           "message_text" : message
