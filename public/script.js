fb04ccac66c37c81e5448e2c0a0d4b615fa45e2f
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-19 12:22:53 -0400

    add post api + change local host to heroku

--- public/script.js
+++ public/script.js
@@ -1,11 +1,12 @@
-const socket = io('http://localhost:3000')
+//import {post_message_data} from "./post_data.js";
+const socket = io('https://chatatbrownhci.herokuapp.com:3000')
 
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 const typing = document.getElementById('typing')
 var indicator_case = 3
-var live_message = ''
+
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
@@ -13,6 +14,8 @@ socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
   typing.innerHTML = ''
+  post_message_data(data.message)
+  //This name is the sender name; you are recipient here
   appendMessage(`${data.name}: ${data.message}`)
 })
 
@@ -57,4 +60,35 @@ function appendMessage(message) {
   const messageElement = document.createElement('div')
   messageElement.innerText = message
   messageContainer.append(messageElement)
+}
+
+function post_message_data(message){
+  var url = "https://nhipj3fca6.execute-api.us-east-1.amazonaws.com/dev/message"
+  var data = {
+      "messages": {
+          "chat_uuid" : "3b547d64-5c37-4f97-a7e2-4254bd9f06d0",
+          "chat_name" : "front-end",
+          "sender_uuid" : "15d18e55-f3c0-476b-bc05-ec12bc36780d",
+          "receiver_uuid" : "f9748683-99f2-4246-b069-7f001ff1b3a4",
+          "message_text" : message
+      }
+  }
+
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept' : '*/*',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      appendMessage('sccess')
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      appendMessage('error')
+    });
 }
