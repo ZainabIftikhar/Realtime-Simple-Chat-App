4a7207b8eab97111fd8819f3590e74580d298a67
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-20 10:29:40 -0400

    show indicator to both users

--- public/script.js
+++ public/script.js
@@ -82,34 +82,3 @@ function appendMessage(message) {
   messageElement.innerText = message
   messageContainer.append(messageElement)
 }
-
-function post_message_data(log_event){
-  var url = "https://nhipj3fca6.execute-api.us-east-1.amazonaws.com/dev/message"
-  var data = {
-      "messages": {
-          "chat_uuid" : "3b547d64-5c37-4f97-a7e2-4254bd9f06d0",
-          "chat_name" : "front-end-version-2",
-          "sender_uuid" : "15d18e55-f3c0-476b-bc05-ec12bc36780d",
-          "receiver_uuid" : "f9748683-99f2-4246-b069-7f001ff1b3a4",
-          "message_text" : log_event
-      }
-  }
-
-  fetch(url, {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept' : '*/*',
-    },
-    body: JSON.stringify(data),
-  })
-  .then((response) => response.json())
-    .then((data) => {
-      console.log('Success:', data);
-      //appendMessage('sccess')
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      //appendMessage('error')
-    });
-}
