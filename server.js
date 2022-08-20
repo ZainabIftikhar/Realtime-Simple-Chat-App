07ebb5b975c6f0994e1b1d8408ea962d380fb112
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-19 21:37:32 -0400

    update server.js w/out stashed

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const io = require('socket.io')(3000)
-=======
-=======
->>>>>>> master
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -12,35 +6,18 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
   res.sendFile(__dirname + '/public/chat.html');
-});
-
-=======
->>>>>>> a5e8ead3ea37a3db45a8a6d05a30e9f9ac1574cd
-  res.sendFile(__dirname + '/index.html');
-=======
-  res.sendFile(__dirname + '/public/chat.html');
->>>>>>> Stashed changes
 });
 
 // Will run the server on port 3000
 // If you change the port make sure to change it in index.html and public/script.js
 // If you plan to deply the server, you will need to change the files public/script.js and index.html to use your public IP
 
->>>>>>> master
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => { 
   console.log(`listening on port ${PORT}`);
 });
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> master
 
 const users = {}
 
@@ -48,27 +25,8 @@ const users = {}
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    post_message_data(`(${name} connected, ${Math.floor(new Date().getTime() / 1000)})`)
-    socket.broadcast.emit('user-connected', name);
-=======
-    socket.broadcast.emit('user-connected', name)  
-    //post_message_data(`(${name} connected, ${Math.floor(new Date().getTime() / 1000)})`)
->>>>>>> Stashed changes
-=======
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    socket.broadcast.emit('user-connected', name)
-=======
     //post_message_data(`(${name} connected, ${Math.floor(new Date().getTime() / 1000)})`)
     socket.broadcast.emit('user-connected', name);
->>>>>>> Stashed changes
-=======
-    post_message_data(`(${name} connected, ${Math.floor(new Date().getTime() / 1000)})`)
-    socket.broadcast.emit('user-connected', name);
->>>>>>> master
->>>>>>> a5e8ead3ea37a3db45a8a6d05a30e9f9ac1574cd
   })
   
   socket.on('send-chat-message', message => {
