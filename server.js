a5e8ead3ea37a3db45a8a6d05a30e9f9ac1574cd
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-19 21:28:14 -0400

    add new html file for chat uuid + sender uuid - currently uses get for form (#5)

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+const io = require('socket.io')(3000)
+=======
+=======
+>>>>>>> master
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -6,6 +12,11 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+=======
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -13,11 +24,16 @@ app.get('/', (req, res) => {
 // If you change the port make sure to change it in index.html and public/script.js
 // If you plan to deply the server, you will need to change the files public/script.js and index.html to use your public IP
 
+>>>>>>> master
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => { 
   console.log(`listening on port ${PORT}`);
 });
+<<<<<<< HEAD
+>>>>>>> Stashed changes
+=======
+>>>>>>> master
 
 const users = {}
 
@@ -25,8 +41,17 @@ const users = {}
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+    socket.broadcast.emit('user-connected', name)
+=======
+    //post_message_data(`(${name} connected, ${Math.floor(new Date().getTime() / 1000)})`)
+    socket.broadcast.emit('user-connected', name);
+>>>>>>> Stashed changes
+=======
     post_message_data(`(${name} connected, ${Math.floor(new Date().getTime() / 1000)})`)
     socket.broadcast.emit('user-connected', name);
+>>>>>>> master
   })
   
   socket.on('send-chat-message', message => {
