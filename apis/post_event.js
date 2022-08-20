63d192e16eb32d8b0bb44ecafb524be5b78c0e0d
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-20 10:30:33 -0400

    make modules for api

---
+++ apis/post_event.js
@@ -0,0 +1,32 @@
+function post_event_message(chat_uuid, room, user_uuid, event_message){
+  var url = "https://nhipj3fca6.execute-api.us-east-1.amazonaws.com/dev/message"
+  var data = {
+      "messages": {
+          "chat_uuid" : chat_uuid,
+          "chat_name" : chat_uuid + room,
+          "sender_uuid" : user_uuid, 
+          "receiver_uuid" : user_uuid, //dummy user for now
+          "message_text" : event_message
+      }
+  }
+
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept' : '*/*',
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+module.exports = {
+  post_event_message
+};
