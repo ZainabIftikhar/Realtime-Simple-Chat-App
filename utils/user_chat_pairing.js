5eccb73edeff681040bd35204b39a5ee81d3b7f2
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-28 00:04:36 -0400

    Create user_chat_pairing.js

---
+++ utils/user_chat_pairing.js
@@ -0,0 +1,17 @@
+var user_chat_pairings = {
+  //Session 1: (P7, P6)
+  '7a1d0803-87b0-45ec-8391-76a97e052967': 
+  ['0f47a1cc-e7ea-4b26-a4a6-1102cdcaa1d6', 'aafb9a91-982f-4dfe-9691-5ac9389c6fbf'],
+  
+  'a5a9223d-1ab9-4060-853b-b8264b514a51':
+  ['f634b3eb-50c9-4dd9-98d8-bf53493d47a9', 'b1594cde-5b04-4038-9b6d-e322013576fa'],
+  
+  '5eedda90-f61e-49a9-a801-46a7d8c69e4a':
+  ['e013ac18-d152-422e-b7d1-6c7378720679', '30cd58bc-d7c6-4acc-b58c-564297f49429'],
+  
+  'dd027cd2-bc58-45fe-ac50-7445614edff0':
+  ['7f418376-1194-4085-8487-69f3260fd14c', 'eb60007d-345a-4afe-8026-3bf76b9cfdb5']
+}
+
+console.log(user_chat_pairings['7a1d0803-87b0-45ec-8391-76a97e052967'][0])
+console.log(user_chat_pairings['7a1d0803-87b0-45ec-8391-76a97e052967'][1])
