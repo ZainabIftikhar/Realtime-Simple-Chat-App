52856ea6bc8c997e6736b7e8ab7d2755e5ac218a
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-31 14:23:42 -0400

    add group 3 participants

--- utils/schedule.js
+++ utils/schedule.js
@@ -70,40 +70,40 @@ const participants_group_2 = {
   "f28f6899-b2e1-4cbb-9ada-286a35b463be": {"user1": p12, "user2": p15, "room": 3, "slot": 3}
 }
 
-const p17 = "0834b0b4-7a02-4ac1-913b-528c1b055595";
-const p18 = "05924938-74cb-4800-a6e3-2bf8989a87d8";
-const p19 = "49c70591-b39a-4228-b292-50d3b1ccfc1f";
-const p20 = "1508ff80-b73d-4433-8ec6-b1da4c267616";
-const p21 = "a6927e4d-31be-459e-8d10-dcc9fdc8f25c";
-const p22 = "72e981e2-8248-46c8-a362-b384050c889b";
-const p23 = "5b85ccb4-beeb-4b1b-8166-2dd285053792";
-const p24 = "b1f03f5d-ce52-4b62-abe5-cb766bb319e2";
+const p17 = "f15fb28c-883f-48c4-ad29-fb6d774e5f4a";
+const p18 = "175b7da9-0610-461d-8c8e-859eb098023f";
+const p19 = "82f059bc-7062-41dd-af64-8c069d892f26";
+const p20 = "1b914e27-7237-4f2f-baf1-dc00e191bd56";
+const p21 = "2b9c4473-882b-4e94-ae5b-bbc5a72635c6";
+const p22 = "95ad94d4-4f91-4e19-b50e-36c48c08bf7f";
+const p23 = "e8a0d0ba-6c57-4a43-823e-6d65fa7bb975";
+const p24 = "14815c4f-d4f0-43e2-8363-91e0a0d8b5f7";
 
 const participants_group_3 = {
   
-  //Group 2 Slot 1
-  "903bcb5e-ebc5-418c-bc11-88006be16d2f": {"user1": p15, "user2": p14, "room": 0, "slot": 0},
-  "6b600a40-7c66-4b02-b55f-59cbc23166b5": {"user1": p9, "user2": p13, "room": 1, "slot": 1},
-  "a0efebfb-1f9c-45cc-979b-9f9b5c002fd5": {"user1": p10, "user2": p12, "room": 2, "slot": 2},
-  "9b388864-8bee-4f9e-943b-69038776465d": {"user1": p11, "user2": p16, "room": 3, "slot": 3},
+  //Group 3 Slot 1
+  "be475b35-d9cc-4b69-90ee-7b27827a01d0": {"user1": p23, "user2": p22, "room": 0, "slot": 0},
+  "40b24539-9aaa-4d06-8221-7ddf5eca1a80": {"user1": p17, "user2": p21, "room": 1, "slot": 1},
+  "ff9dc3a9-d80d-4b12-b10e-ee302e166b3b": {"user1": p18, "user2": p20, "room": 2, "slot": 2},
+  "b8a0b9d5-43f4-4f42-8d1c-c97df3b01519": {"user1": p19, "user2": p24, "room": 3, "slot": 3},
   
-  //Group 2 Slot 2
-  "b7791ad1-bffd-4e96-925a-7a0ea295c529": {"user1": p9, "user2": p16, "room": 0, "slot": 0},
-  "c27ea70a-e026-4297-bc8e-3469fb96a5e7": {"user1": p15, "user2": p12, "room": 1, "slot": 1},
-  "36038186-b15f-4832-b309-74cbff0724da": {"user1": p11, "user2": p13, "room": 2, "slot": 2},
-  "c1294a37-4133-406d-8da9-9354c3eae6ea": {"user1": p10, "user2": p14, "room": 3, "slot": 3},
+  //Group 3 Slot 2
+  "1ea30bf6-a23b-48fe-bcec-f985fb9440ba": {"user1": p17, "user2": p24, "room": 0, "slot": 0},
+  "db06385f-8b32-4ec8-9117-9d12cd4a96f3": {"user1": p23, "user2": p20, "room": 1, "slot": 1},
+  "c3741b30-a828-4f91-8d83-dad6ccc1bda3": {"user1": p19, "user2": p21, "room": 2, "slot": 2},
+  "b598a47b-04b4-4347-a196-746072f32b62": {"user1": p18, "user2": p22, "room": 3, "slot": 3},
   
-  //Group 2 Slot 3
-  "8f80bb47-c8ef-4aa5-8869-4b0f80790f58": {"user1": p12, "user2": p11, "room": 0, "slot": 0},
-  "627507ea-349b-469d-9382-e45f71b069b1": {"user1": p10, "user2": p14, "room": 1, "slot": 1},
-  "3766ef88-2621-4f0e-b138-a6f977b704e7": {"user1": p15, "user2": p16, "room": 2, "slot": 2},
-  "2c977389-8ee4-464f-82c6-9c9165c7d1eb": {"user1": p9, "user2": p13, "room": 3, "slot": 3},
+  //Group 3 Slot 3
+  "951c6cc4-3cd1-4954-b206-e3d565015e35": {"user1": p20, "user2": p19, "room": 0, "slot": 0},
+  "f79d3a4f-dd58-49d5-9e41-bb3949513ea4": {"user1": p18, "user2": p22, "room": 1, "slot": 1},
+  "490838ad-e8c6-46b7-895d-022cabaf1c5a": {"user1": p23, "user2": p24, "room": 2, "slot": 2},
+  "2bb12528-ce14-48c7-983c-ba12e8204226": {"user1": p17, "user2": p21, "room": 3, "slot": 3},
   
-  //Group 2 Slot 4
-  "c7d77e3b-8474-40fb-8a7c-6416a30980c0": {"user1": p10, "user2": p13, "room": 0, "slot": 0},
-  "35fbcd11-7573-457a-8c91-e8182ea5267d": {"user1": p11, "user2": p16, "room": 1, "slot": 1},
-  "b4c6a207-f1ef-4996-ae44-8f1535b0da42": {"user1": p9, "user2": p14, "room": 2, "slot": 2},
-  "f28f6899-b2e1-4cbb-9ada-286a35b463be": {"user1": p12, "user2": p15, "room": 3, "slot": 3}
+  //Group 3 Slot 4
+  "cdbef206-8465-43fc-b294-3cb77fca69af": {"user1": p18, "user2": p21, "room": 0, "slot": 0},
+  "256e3a26-6c2e-4794-bb64-d72c7a72b07b": {"user1": p19, "user2": p24, "room": 1, "slot": 1},
+  "75f28d72-40c1-4659-b16a-2fa6549a495e": {"user1": p17, "user2": p22, "room": 2, "slot": 2},
+  "23871d97-c527-4ece-b84b-482fba73f7e0": {"user1": p20, "user2": p23, "room": 3, "slot": 3}
 }
 
 module.exports = {
