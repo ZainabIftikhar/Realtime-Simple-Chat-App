d4414320e9e5bdecb9ac1b3fe8f593375deb5d50
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-30 21:22:14 -0400

    fix undefined error in authenticate

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,4 +1,4 @@
-const { participants_group_1} = require('./utils/schedule');
+const { participants_group_1} = require('./schedule');
 //const { participants_group_2} = require('./utils/schedule');
 //const { participants_group_3} = require('./utils/schedule');
 var tc = require("timezonecomplete");   
@@ -19,21 +19,16 @@ const diff_from_group_3 = current_time.diff(group3_slot).minutes()
 function authenticate(chat_uuid, user_uuid, room){
   if (diff_from_group_1 > 0 && diff_from_group_1 < 60){
     var slot = participants_group_1[chat_uuid]
-    if ((slot.user1 === user_uuid || slot.user2 === user_uuid) && slot.room === room){
+    if (typeof slot === 'undefined'){
+      return allow_access
+    }
+    else if ((slot.user1 == user_uuid || slot.user2 == user_uuid) && slot.room == room){
       allow_access = true 
     }
   } 
   else if (diff_from_group_2 > 0 && diff_from_group_2 < 60) {
-    var slot = participants_group_2[chat_uuid]
-    if ((slot.user1 === user_uuid || slot.user2 === user_uuid) && slot.room === room){
-      allow_access = true 
-    }
   }
   else if (diff_from_group_3 > 0 && diff_from_group_3 < 60) {
-    var slot = participants_group_3[chat_uuid]
-    if ((slot.user1 === user_uuid || slot.user2 === user_uuid) && slot.room === room){
-      allow_access = true 
-    }
   }
   return allow_access
 }
