1520dcf83ff724166b6f8e18b65a51f7edb513ca
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-30 20:42:39 -0400

    Create schedule.js - for group 1 participants

---
+++ utils/schedule.js
@@ -0,0 +1,39 @@
+const p1 = "f634b3eb-50c9-4dd9-98d8-bf53493d47a9";
+const p2 = "e013ac18-d152-422e-b7d1-6c7378720679";
+const p3 = "7f418376-1194-4085-8487-69f3260fd14c";
+const p4 = "30cd58bc-d7c6-4acc-b58c-564297f49429";
+const p5 = "b1594cde-5b04-4038-9b6d-e322013576fa";
+const p6 = "aafb9a91-982f-4dfe-9691-5ac9389c6fbf";
+const p7 = "0f47a1cc-e7ea-4b26-a4a6-1102cdcaa1d6";
+const p8 = "eb60007d-345a-4afe-8026-3bf76b9cfdb5";
+
+const participants_group_1 = {
+  
+  //Group 1 Slot 1
+  "7a1d0803-87b0-45ec-8391-76a97e052967": {"user1": p7, "user2": p6, "room": 0, "slot": 0},
+  "a5a9223d-1ab9-4060-853b-b8264b514a51": {"user1": p1, "user2": p5, "room": 1, "slot": 1},
+  "5eedda90-f61e-49a9-a801-46a7d8c69e4a": {"user1": p2, "user2": p4, "room": 2, "slot": 2},
+  "dd027cd2-bc58-45fe-ac50-7445614edff0": {"user1": p3, "user2": p8, "room": 2, "slot": 3},
+  
+  //Group 1 Slot 2
+  "50496918-caf4-483f-b80f-26cd0776c9ed": {"user1": p1, "user2": p8, "room": 0, "slot": 0},
+  "718c17fd-f19f-4c2d-a10b-4e1c56280dba": {"user1": p7, "user2": p4, "room": 1, "slot": 1},
+  "7b2dfe19-9f03-4c6d-9558-43890056aef6": {"user1": p3, "user2": p5, "room": 2, "slot": 2},
+  "b99351a8-2423-4a23-b494-87603281583a": {"user1": p2, "user2": p6, "room": 3, "slot": 3},
+  
+  //Group 1 Slot 3
+  "153db96c-289b-4645-8f55-c7d1a3884a63": {"user1": p4, "user2": p3, "room": 0, "slot": 0},
+  "00b2c1b9-bb4f-4382-bb71-5bb0d63a6d23": {"user1": p2, "user2": p6, "room": 1, "slot": 1},
+  "fe31c076-6087-4b05-b261-d2d5207a3808": {"user1": p7, "user2": p8, "room": 2, "slot": 2},
+  "927072ee-adc8-471f-94eb-2be5aec8f873": {"user1": p1, "user2": p5, "room": 3, "slot": 3},
+  
+  //Group 1 Slot 4
+  "6c24e710-a220-48cb-a081-e08700257a1a": {"user1": p2, "user2": p5, "room": 0, "slot": 0},
+  "f666cb07-74fb-4516-a735-1521c25b2a16": {"user1": p3, "user2": p8, "room": 1, "slot": 1},
+  "4be66f0c-6795-4221-b013-58b85e3b76bc": {"user1": p1, "user2": p6, "room": 2, "slot": 2},
+  "0df46f64-5fe1-476b-879e-a5f8a7577f54": {"user1": p4, "user2": p7, "room": 3, "slot": 3}
+}
+
+module.exports = {
+  participants_group_1
+}
