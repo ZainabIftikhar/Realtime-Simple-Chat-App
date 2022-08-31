4a51e4e89cc879a84eca09ca0c95f4be4231183c
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-08-31 16:06:33 -0400

    Create problems

---
+++ utils/problems.js
@@ -0,0 +1,53 @@
+const p1 = `You have just crash landed in the desert. 
+Before the plane caught fire, your group was able to save the following 7 items on the desk:
+• Torch with 4 battery-cells 
+• Folding knife 
+• Plastic raincoat (large size)  
+• Parachute (red & white) 
+• 2 liters of water per person 
+• Overcoat (for everyone) 
+• A cosmetic mirror
+In your pairs, your task is to rank the top 3 items according to their importance to your survival in the desert, starting with 1 for the most important. 
+You MUST come to an agreement together.`
+
+
+const p2 = `You are a member of a space crew whose ship crashed far away from the rendezvous point. 
+Below are listed the 7 items left intact and undamaged after landing: 
+• Food concentrate 
+• 50 feet of nylon rope 
+• Two 100 lb. tanks of oxygen
+• Stellar map
+• 20 liters of water 
+• First aid kit, including injection needle
+• Solar-powered FM receiver-transmitter 
+In your pairs, your task is to rank the top 3 items according to their importance to your survival 
+or your crew in allowing them to reach the rendezvous point, starting with 1 for the most important.
+You MUST come to an agreement together.` 
+
+
+const p3 = `You are adrift on a private yacht in the South Pacific. As a consequence of a fire of unknown origin, much of the yacht and its contents have been destroyed. 
+The yacht is now slowly sinking. Your location is unclear because of the destruction of critical navigational equipment and because you and the crew were distracted trying to bring the fire under control.
+Your best estimate is that you are approximately one thousand miles south-southwest of the nearest land. Below is a list of 7 items that are intact and undamaged after the fire:
+• Shaving Mirror 
+• 25 liter container of Water 
+• Case of Army Rations 
+• 10 liter can of Oil/Petrol mixture 
+• 20 sq. ft. of opaque Plastic Sheeting 
+• 2 boxes of Chocolate Bars 
+• An ocean Fishing Kit & Pole
+In your pairs, your task is to rank the top 3 items according to their importance to your survival, starting with 1 for the most important. 
+You MUST come to an agreement together.` 
+
+
+const p4 = `You and your companions have just survived the crash of a small plane. Both the pilot and co-pilot were killed in the crash. 
+It is mid-January , and you are in Northern Canada. The daily temperature is 25 below zero and the night time temperature is 40 below zero. 
+There is snow on the ground, and the countryside is wooded with several creeks criss-crossing the area. The nearest town is 20 miles away.
+Your group of survivors managed to salvage the 7 following items:
+• A ball of steel wool
+• A small ax
+• Can of Crisco shortening
+• Cigarette lighter (without fluid)
+• Extra shirt and pants for each survivor
+• 20 x 20 ft. piece of heavy-duty canvas
+• Family-size chocolate bars (one per person)
+Your task as a group is to list the above 7 items in order of importance for your survival. List the uses for each. You MUST come to agreement as a group.` 
