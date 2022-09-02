5b485d316738d0d25e4796acb3490fbb9ccf15ea
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-09-02 17:44:11 -0400

    Revert "people"
    This reverts commit 3ddaac0a60721352b14b51fc992722d41261741e.

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => { console.log(`listening on port ${PORT}`);});
 
-const a = true;
 
 io.on('connection', socket => {
   socket.on('new-user', ({name, chat_uuid, user_uuid, room}) => {
