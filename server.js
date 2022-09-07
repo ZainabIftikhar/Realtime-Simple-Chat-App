097e858cb22894e26c0b8495587cb409ec792c42
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-09-07 00:17:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ server.listen(PORT, () => { console.log(`listening on port ${PORT}`);});
 io.on('connection', socket => {
   socket.on('new-user', ({name, chat_uuid, user_uuid, room}) => {
     
-    const [authenticate_flag, task] = authenticate(chat_uuid, user_uuid, room)
+    //const [authenticate_flag, task] = authenticate(chat_uuid, user_uuid, room)
+    const [authenticate_flag, task] = [true, '']
+    
     if (authenticate_flag == false) {
       var destination = '/error.html';
       socket.emit(socket.id).emit('redirect', destination);
