6f9d4a256e6fb87dde46399cabc6e3605f7d995d
ZainabIftikhar <zainabiftikhar87@gmail.com>
2022-09-07 00:19:40 -0400

    Revert "Update authenticate.js"
    This reverts commit 00cdd99ff9855a9cf6d125da42a3de3edd6d5316.

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -15,7 +15,9 @@ const group1_slot = new tc.DateTime("2022-08-31T22:00:00 America/New_York")
 const group2_slot = new tc.DateTime("2022-08-31T22:00:00 America/New_York")
 const group3_slot = new tc.DateTime("2022-08-31T22:00:00 America/New_York")
 
-const current_time = new tc.DateTime();
+const current_time = new tc.DateTime("2022-08-31T22:00:00 America/New_York");
+
+const a  = true;
 
 var task_dict = {
   1: task1,
@@ -33,17 +35,18 @@ function authenticate(chat_uuid, user_uuid, room){
   if ((diff_from_group_1 => 0) && (diff_from_group_1 <= 60)){
     var slot = participants_group_1[chat_uuid]
     if (typeof slot === 'undefined'){
-      return [allow_access, ''] 
+      //return [allow_access, ''] 
     }
     else if ((slot.user1 == user_uuid || slot.user2 == user_uuid) && slot.room == room){
       allow_access = true
       return [allow_access, task_dict[slot.problem]] 
     }
   } 
+  //else if ((diff_from_group_2 => 0) && (diff_from_group_2 <= 60)) {
   if ((diff_from_group_2 => 0) && (diff_from_group_2 <= 60)) {
     var slot = participants_group_2[chat_uuid]
     if (typeof slot === 'undefined'){
-      return [allow_access, ''] 
+      //return [allow_access, ''] 
     }
     else if ((slot.user1 == user_uuid || slot.user2 == user_uuid) && slot.room == room){
       allow_access = true
@@ -51,9 +54,10 @@ function authenticate(chat_uuid, user_uuid, room){
     }
   }
   if ((diff_from_group_3 => 0) && (diff_from_group_3 <= 60)) {
+  //else if ((diff_from_group_3 => 0) && (diff_from_group_3 <= 60)) {
     var slot = participants_group_3[chat_uuid]
     if (typeof slot === 'undefined'){
-      return [allow_access, ''] 
+      //return [allow_access, ''] 
     }
     else if ((slot.user1 == user_uuid || slot.user2 == user_uuid) && slot.room == room){
       allow_access = true
